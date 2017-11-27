d9e0386d8495a8d250b5118809e94f3202afa3f0
vishnu-suresh10 <33134047+vishnu-suresh10@users.noreply.github.com>
2017-11-26 20:23:33 -0500

    Add files via upload

---
+++ src/CalorieFinder.java
@@ -0,0 +1,46 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+
+public class CalorieFinder {
+    private String allItemsOnMenu;
+
+    public CalorieFinder(){
+        allItemsOnMenu = initializeCalorie();
+    }
+
+    public String initializeCalorie() { //Gets Source Code from Website that has nutritional information of all items on Chipotle's menu
+        String allItemsOnMenu = "";
+        try {
+            final Document document = Jsoup.connect("https://www.thrillist.com/health/nation/chipotle-calories-menu-nutrition-facts").get(); //Gets source code of website
+            allItemsOnMenu = document.select("section.body-text.u-color--dark-gray.u-color--health-links.font--body.has-mobile-padding.is-standard li").text(); //Selects text on the website that is a food
+        }
+
+        catch (IOException ex) {
+            System.out.println("***Cannot Connect To Website***");
+        }
+
+        finally {
+            return allItemsOnMenu;
+        }
+    }
+
+    public int getCalories(String food){
+        String calorie = "";
+        int CALORIES_IN_FOOD = 0;
+
+        food = food.toLowerCase();
+
+        int indexOfFood = allItemsOnMenu.indexOf(food);
+        int indexOfSemicolon = allItemsOnMenu.indexOf("calories",indexOfFood );
+
+        String subItem = allItemsOnMenu.substring(indexOfFood, indexOfSemicolon-1);
+
+        calorie = subItem.replaceAll(food+": ", "");
+
+        CALORIES_IN_FOOD = Integer.parseInt(calorie);
+
+        return CALORIES_IN_FOOD;
+
+    }
+}
